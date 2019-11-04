9c360baf3d8a758875558ff21811f6021fcb5b74
zhangfengzhou <1120335370@qq.com>
2019-11-04 19:01:58 +0800

    整理代码

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/zhangfengzhou/Develop/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/zhangfengzhou/Documents/GitHub/FlutterLearn"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/zhangfengzhou/Develop/Flutter/flutter/bin/cache/artifacts/engine/ios"
